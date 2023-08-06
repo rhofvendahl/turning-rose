ab235b3b65f4256b295f30fde9bc3ed88c31e535
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-08-06 14:54:08 -0700

    feat: make rotation smoother

--- tr-web/src/App.tsx
+++ tr-web/src/App.tsx
@@ -12,23 +12,12 @@ import { getLoadedRatio } from "./shared"
 // This doesn't seem ideal, but even useEffect with empty inputs within App seems to run twice over the app"s lifecycle - no good.
 let hasInitialized = false;
 
-const radToDog = (rad: number): number => {
-  return rad * 360 / (2 * Math.PI);
-};
-
 const degToRad = (deg: number): number => {
   return deg * 2 * Math.PI / 360;
 };
 
-// Yes I am aware that using loading ratio to drive rotation is silly. There are other ways to do it. This is easy.
-// UPDATE: And here I was wonderwing why it moves so slowly on first load...
-const getRotation = (frames: Frame[], start: number): [number, number, number] => {
+const getRotation = (start: number): [number, number, number] => {
   const delta = (Date.now() - start) / 1000;
-  console.log("DELTA", delta)
-  const loadedRatio = getLoadedRatio(frames);
-  if (loadedRatio >= 1) {
-    return [0, 0, 0];
-  }
   return [0, degToRad(delta * 4), 0];
 };
 
@@ -36,8 +25,9 @@ const App = () => {
   const [frames, setFrames] = useState<Frame[]>([]);
   const [currentFrame, setCurrentFrame] = useState<Frame | null>(null);
   const currentFrameRef = useRef(currentFrame);
+
+  const [rotation, setRotation] = useState<[number, number, number]>([0, 0, 0]);
   const rotationIntervalRef = useRef(0);
-  const startRef = useRef(Date.now())
 
   // Provide a way to access current frame within frame loading functions, so that the appropriate frame is loaded next
   useEffect(() => {
@@ -50,19 +40,29 @@ const App = () => {
       useFrames({ setFrames, currentFrameRef, setCurrentFrame });
 
       // Put initial rotation in motion
+      const start = Date.now()
       rotationIntervalRef.current = window.setInterval(() => {
-
-      }, 1);
+        const rotation = getRotation(start);
+        setRotation(rotation);
+      }, 50);
       hasInitialized = true;
     }
   }, []);
+
+  // Stop rotation once loaded
+  useEffect(() => {
+    const loadedRatio = getLoadedRatio(frames);
+    if (loadedRatio >= 1) {
+      window.clearInterval(rotationIntervalRef.current);
+    }
+  }, [frames]);
 
   return (
     <div id="content-container">
       <Canvas id="canvas">
         <ambientLight intensity={1} />
         <OrbitControls />
-        <mesh rotation={getRotation(frames, startRef.current)} position={[0, .3, 0]}>
+        <mesh rotation={rotation} position={[0, .3, 0]}>
           { currentFrame !== null && getLoadedRatio(frames) >= 1 ? <ViewFrame frame={currentFrame} /> : <ViewFrame frame={frames[frames.length - 1]} />}
         </mesh>
       </Canvas>
